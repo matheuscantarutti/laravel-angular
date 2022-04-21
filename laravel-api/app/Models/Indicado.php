f5a24118f412a4679d2425038710a3a8ce9ed374
Matheus <matheuscantarutti@gmail.com>
2022-04-21 17:24:27 -0300

    adição de campos na model Indicado e migration

--- laravel-api/app/Models/Indicado.php
+++ laravel-api/app/Models/Indicado.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Indicado extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'telefone',
+        'email',
+        'status_indicacao'
+    ];
 }
--- laravel-api/database/migrations/2022_04_21_194029_create_indicados_table.php
+++ laravel-api/database/migrations/2022_04_21_194029_create_indicados_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\StatusIndicacao;
 
 return new class extends Migration
 {
@@ -16,6 +17,11 @@ return new class extends Migration
         Schema::create('indicados', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('nome');
+            $table->string('cpf', 11);
+            $table->text('telefone');
+            $table->text('email');
+            $table->tinyInteger('status_indicacao')->unsigned()->default(StatusIndicacao::Iniciada);
         });
     }
 
