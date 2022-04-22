422585be9c0f056bb099adcf76a71d8f26984f80
Matheus <matheuscantarutti@gmail.com>
2022-04-22 18:02:23 -0300

    criação das rotas básicas CRUD

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndicadoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('indicados', [IndicadoController::class, 'index']);
+Route::get('indicado/{indicado}', [IndicadoController::class, 'show']);
+Route::post('indicado', [IndicadoController::class, 'store']);
+Route::put('indicado/{indicado}', [IndicadoController::class, 'update']);
+Route::delete('indicado/{indicado}', [IndicadoController::class,'destroy']);
