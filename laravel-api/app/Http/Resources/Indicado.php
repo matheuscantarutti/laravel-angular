08249f3cb42f1a39ac392b83d336c0748ae78b0c
Matheus <matheuscantarutti@gmail.com>
2022-04-22 18:02:44 -0300

    criação de resource da model Indicado

---
+++ laravel-api/app/Http/Resources/Indicado.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Indicado extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'cpf' => $this->cpf,
+            'telefone' => $this->telefone,
+            'email' => $this->email,
+            'status_indicacao' => $this->status_indicacao
+        ];
+    }
+}
