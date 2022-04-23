b87d165ed381ec68fbbdc62cab0f52095eaba0ee
Matheus <matheuscantarutti@gmail.com>
2022-04-22 21:12:41 -0300

    criação do controller Indicado

---
+++ laravel-api/app/Http/Controllers/IndicadoController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreIndicadoRequest;
+use App\Http\Requests\UpdateIndicadoRequest;
+use App\Models\Indicado;
+use App\Http\Resources\Indicado as IndicadoResource;
+
+class IndicadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $indicados = Indicado::all();
+        return IndicadoResource::collection($indicados);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreIndicadoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreIndicadoRequest $request)
+    {
+        $request->validated();
+
+        $indicado = Indicado::create($request->all());
+
+        $indicado->status_indicacao = 1;
+
+        return response()->json($indicado, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Indicado  $indicado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Indicado $indicado)
+    {
+        return new IndicadoResource( $indicado );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateIndicadoRequest  $request
+     * @param  \App\Models\Indicado  $indicado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateIndicadoRequest $request, Indicado $indicado)
+    {
+        //$request->validated();
+
+        $indicado->update($request->all());
+
+        return response()->json($indicado, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Indicado  $indicado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Indicado $indicado)
+    {
+        $indicado->delete();
+
+        return response()->json(null, 204);
+    }
+}
