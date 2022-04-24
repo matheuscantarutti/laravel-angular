87c757d6b1f485b70b1a463342aab548e1a63603
Matheus <matheuscantarutti@gmail.com>
2022-04-24 12:23:52 -0300

    configuração inicial do front-end

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { AddIndicadoComponent } from 'src/app/components/add-indicado/add-indicado.component';
+import { UpdateIndicadoComponent } from 'src/app/components/update-indicado/update-indicado.component';
+import { IndicadosListComponent } from 'src/app/components/indicado-list/indicado-list.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'indicado-list' },
+  { path: 'add-indicado', component: AddIndicadoComponent },
+  { path: 'update-indicado/:id', component: UpdateIndicadoComponent },
+  { path: 'indicado-list', component: IndicadosListComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { IndicadoService } from './indicado.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddIndicadoComponent } from './components/add-indicado/add-indicado.component';
+import { UpdateIndicadoComponent } from './components/update-indicado/update-indicado.component';
+import { IndicadosListComponent } from './components/indicado-list/indicado-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIndicadoComponent,
+    UpdateIndicadoComponent,
+    IndicadosListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [IndicadoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
