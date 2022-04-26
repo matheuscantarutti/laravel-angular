ca911b408a5883d64d9902268f723f00798f6540
Matheus <matheuscantarutti@gmail.com>
2022-04-26 01:48:04 -0300

    ajuste da model

--- laravel-api/app/Models/Indicado.php
+++ laravel-api/app/Models/Indicado.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\StatusIndicacao;
 
 class Indicado extends Model
 {
@@ -15,5 +16,9 @@ class Indicado extends Model
         'telefone',
         'email',
         'status_indicacao'
+    ];
+
+    protected $attributes = [
+        'status_indicacao' => StatusIndicacao::Iniciada
     ];
 }
